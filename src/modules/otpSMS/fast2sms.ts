364fc626e558f7946eef4a732655d73f8b0509c0
Vipin Bohra <vipinbohra321@gmail.com>
2025-04-02 17:53:46 +0530

    feat: updated fast2Sms service

--- src/modules/otpSMS/fast2sms.ts
+++ src/modules/otpSMS/fast2sms.ts
@@ -2,9 +2,19 @@ import axios from 'axios'
 
 export const sendFastOTPSMS = async (phoneNumber: string, verificationCode: string) => {
   const apiKey = process.env.FAST_SMS_API_KEY
-  const url = `https://www.fast2sms.com/dev/bulkV2?authorization=${apiKey}&variables_values=${verificationCode}&route=otp&numbers=${phoneNumber}`
+
+  const params = {
+    authorization: apiKey,
+    sender_id: 'Nara',
+    message: 182637,
+    variables_values: `${verificationCode}|`,
+    route: 'dlt',
+    numbers: phoneNumber,
+  }
+
+  const url = `https://www.fast2sms.com/dev/bulkV2`
   try {
-    const response = await axios.get(url)
+    const response = await axios.get(url, { params })
     console.log(response.data)
     return true
   } catch (err: any) {
