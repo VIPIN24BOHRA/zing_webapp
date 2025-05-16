afe22876281d31f4630517e4b06049275c46a9cb
Vipin Bohra <vipinbohra321@gmail.com>
2025-05-16 21:09:26 +0530

    feat: added middleware to rate limit

---
+++ src/middleware/rateLimiter.ts
@@ -0,0 +1,32 @@
+const rateLimitMap = new Map()
+
+export default function rateLimitMiddleware(handler: any) {
+  return (req: any, res: any) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    const limit = 5 // Limiting requests to 5 per minute per IP
+    const windowMs = 60 * 60 * 1000 // 1 hour
+
+    if (!rateLimitMap.has(ip)) {
+      rateLimitMap.set(ip, {
+        count: 0,
+        lastReset: Date.now(),
+      })
+    }
+
+    const ipData = rateLimitMap.get(ip)
+
+    if (Date.now() - ipData.lastReset > windowMs) {
+      ipData.count = 0
+      ipData.lastReset = Date.now()
+    }
+
+    if (ipData.count >= limit) {
+      console.log(ip, ipData, ipData.count)
+      return res.status(429).send('Too Many Requests')
+    }
+
+    ipData.count += 1
+
+    return handler(req, res)
+  }
+}
